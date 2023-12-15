471dc00c00156ba7ef891edee6f51a64bb2c0054
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-12-15 22:27:03 +0200

    display list posts in content single post

--- functions.php
+++ functions.php
@@ -781,3 +781,35 @@ function my_auth($user, $username, $password)
 	}
 	return $user;
 }
+
+// Блок записей в контенте страниц
+function wfm_add_category_posts($atts)
+{
+	if (empty($atts['id'])) return;
+	$per_page = !empty($atts['count']) ? (int)$atts['count'] : 3;
+	if ($per_page < 1) $per_page = 3;
+	
+	$cats_id = explode(',', $atts['id']);
+	
+	$get_posts = new WP_Query(
+		array(
+			'category__in' => $cats_id,
+			'posts_per_page' => $per_page,
+		),
+	);
+
+	$content = '';
+
+	if ($get_posts->have_posts()) {
+		$content .= '<div class="insert-posts">';
+		while ($get_posts->have_posts()) {
+			$get_posts->the_post();
+			$content .= '<p>' . get_the_title() . '</p>';
+		}
+		$content .= '</div>';
+	}
+
+	return $content;
+}
+
+add_shortcode('wfm-cats', 'wfm_add_category_posts');
